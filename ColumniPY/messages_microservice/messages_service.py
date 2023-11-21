774adf9b9119a14ec7c9134bf883aa1d92eb74ac
Maxwell Gonsalves <mag2352@columbia.edu>
2023-11-20 19:13:38 -0500

    added preliminary changes for aggregator.

--- ColumniPY/messages_microservice/messages_service.py
+++ ColumniPY/messages_microservice/messages_service.py
@@ -3,39 +3,48 @@ import psycopg2
 
 app = Flask(__name__)
 
-conn = psycopg2.connect('https://messages-microservice.ue.r.appspot.com/')
-cur = conn.cursor()
+MESSAGES_URL = 'https://messages-microservice.ue.r.appspot.com/'
 
 PAGE_SIZE = 10
+
+class MessageModel(BaseModel):
+    userMessageID: int
+    userID: int
+    messageID: int
+    messageContents: str
+    creationDT: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "userMessageID": 1,
+                    "userID": 1,
+                    "messageID": 1,
+                    "messageContents": "Hi! (Potentially Encrypted).",
+                    "creationDT": "10/3/23 16:25"
+                }
+            ]
+        }
+    }
 
 # API Routes
 @app.route('/api/messages/<int:user_id>', methods=['GET'])
 def get_messages(user_id):
-    page = request.args.get('page', 1, type=int)
-
     offset = (page - 1) * PAGE_SIZE
-
-    query = """
-            SELECT * FROM messages
-            WHERE user_id = %s
-            ORDER BY timestamp DESC
-            LIMIT %s OFFSET %s
-        """
-    cur.execute(query, (user_id, PAGE_SIZE, offset))
-    messages = cur.fetchall()
-    return jsonify({"messages": messages})
+    return requests.get(MESSAGES_URL+'api/messages?userID='+str(user_id)+'&offset='+str(offset)+'&limit=10').json
 
 
-@app.route('/api/messages/<int:user_id>/newMessage', methods=['POST'])
-def new_message(user_id):
+@app.route('/api/messages/newMessage', methods=['POST'])
+def new_message(request: MessageModel, user_id):
     # Implement logic to create a new message for a user
-    return jsonify({"message": f"New message created for user {user_id}"})
+    return requests.post(MESSAGES_URL+'api/messages/newMessage', json = request)
 
 
 @app.route('/api/messages/<int:user_id>/<int:thread_id>', methods=['GET'])
 def get_thread_messages(user_id, thread_id):
     # Implement logic to retrieve and display messages for a specific thread
-    return jsonify({"message": f"Displaying messages for user {user_id} in thread {thread_id}"})
+    return requests.get(MESSAGES_URL+'api/messages?userID='+str(user_id)+'&messageThreadID='+str(thread_id)+'&offset='+str(offset)+'&limit=10').json
 
 
 # Run the application
