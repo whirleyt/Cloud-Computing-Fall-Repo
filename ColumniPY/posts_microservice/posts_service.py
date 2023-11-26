ea7e89c40bbe7418d63803986f57558d74b03e02
swasthi-prao <145594574+swasthi-prao@users.noreply.github.com>
2023-11-25 19:59:32 -0500

    Update posts_service.py

--- ColumniPY/posts_microservice/posts_service.py
+++ ColumniPY/posts_microservice/posts_service.py
@@ -1,39 +1,44 @@
-from flask import Flask, request, jsonify
-import psycopg2
+class PostModel(BaseModel):
+    userPostID: int
+    userID: int
+    postID: int
+    postThreadID: int
+    postContent: str
+    dateOfCreation: str
 
-app = Flask(__name__)
-
-conn = psycopg2.connect('https://icy-tree-030c60010.4.azurestaticapps.net/')
-cur = conn.cursor()
-
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+           
+                    "userPostID":1,
+                    "userID": 1,
+                    "postID": 1,
+                    "postThreadID": 1,
+                    "postContent": "Hey, so excited to connect after so long!",
+                    "dateOfCreation": "09/15/23 14:33"
+                }
+            ]
+        }
+    }
 
 # API Routes
-@app.route('/api/posts/<int:post_type_id>', methods=['GET'])
-def get_posts_by_type(post_type_id):
-    # Implement logic to retrieve and display posts by post type
-    # Replace the following line with your actual database query
-    return jsonify({"message": f"Displaying posts for post type {post_type_id}"})
+@app.route('/api/posts/<int:user_id>', methods=['GET'])
+def get_posts(user_id):
+    offset = (page - 1) * PAGE_SIZE
+    return requests.get(POSTS_URL+'api/posts?userID='+str(user_id)+'&offset='+str(offset)+'&limit=10').json
 
 
-@app.route('/api/posts/<int:post_type_id>/newPost', methods=['POST'])
-def create_post(post_type_id):
-    # Implement logic to create a new post for a specific post type
-    # Replace the following line with your actual database insertion
-    return jsonify({"message": f"New post created for post type {post_type_id}"})
+@app.route('/api/posts/newPost', methods=['POST'])
+def new_post(request: postModel, user_id):
+    # Icreate a new post for a user
+    return requests.post(POSTS_URL+'api/posts/newPost', json = request)
 
 
-@app.route('/api/posts/<int:post_type_id>/<int:post_id>/likes', methods=['GET'])
-def get_post_likes(post_type_id, post_id):
-    # Implement logic to retrieve and display likes for a post
-    # Replace the following line with your actual database query
-    return jsonify({"message": f"Displaying likes for post {post_id} of type {post_type_id}"})
-
-
-@app.route('/api/posts/<int:post_type_id>/<int:post_id>/comments', methods=['GET'])
-def get_post_comments(post_type_id, post_id):
-    # Implement logic to retrieve and display comments for a post
-    # Replace the following line with your actual database query
-    return jsonify({"message": f"Displaying comments for post {post_id} of type {post_type_id}"})
+@app.route('/api/posts/<int:user_id>/<int:thread_id>', methods=['GET'])
+def get_thread_posts(user_id, thread_id):
+    # etrieve and display post for a specific thread
+    return requests.get(POSTS_URL+'api/posts?userID='+str(user_id)+'&postThreadID='+str(thread_id)+'&offset='+str(offset)+'&limit=10').json
 
 
 # Run the application
